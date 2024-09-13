be1e0f8b8afd4e19e96bdc3eae4585b2914bb598
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-13 08:54:51 -0400

    Add project files.

---
+++ Topic 5 - Decision Structure Assignments/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5___Decision_Structure_Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
