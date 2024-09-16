19b2e2c7e21a3ebe4c3b9be63735f10394fe3fd9
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-16 09:23:32 -0400

    Finished

--- Topic 5 - Decision Structure Assignments/Program.cs
+++ Topic 5 - Decision Structure Assignments/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int parkingCost;
+            int parkingCost, hurricaneCategory;
             double angle, parkingMinutes;
 
             parkingCost = 4;
@@ -59,6 +59,40 @@
             }
 
             Console.WriteLine("It'll cost you " + parkingCost.ToString("C") + " to park here!");
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.Write("Please enter in a hurricane category and I will tell you the wind speeds: ");
+            while (!Int32.TryParse(Console.ReadLine(), out hurricaneCategory));
+            {
+                Console.WriteLine("Invalid input, please try again... ");
+            }
+
+            Console.WriteLine();
+
+            switch (hurricaneCategory)
+            {
+                case 1:
+                    Console.WriteLine("Wind speed is: 74-95 mph or 64-82 kt or 119-153 km/hr");
+                    break;
+                case 2:
+                    Console.WriteLine("Wind speed is: 96-110 mph or 83-95 kt or 154-177 km/hr");
+                    break;
+                case 3:
+                    Console.WriteLine("Wind speed is: 111-130 mph or 96-113 kt or 178-209 km/hr");
+                    break;
+                case 4:
+                    Console.WriteLine("Wind speed is: 131-155 mph or 114-135 kt or 210-249 km/hr");
+                    break;
+                case 5:
+                    Console.WriteLine("Wind speed is greater than: 155 mph or 135 kt or 249 km/hr");
+                    break;
+                default:
+                    Console.WriteLine("That is not a hurricane category...");
+                    break;
+            }
         }
     }
 }
